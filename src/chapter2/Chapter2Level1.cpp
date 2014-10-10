423f3a8cd4360bd1da6e0c3f3a90826fa04c5ac6
Slack-Moehrle <slackmoehrle@gmail.com>
2014-10-10 09:42:12 -0700

    continued edits

--- src/chapter2/Chapter2Level1.cpp
+++ src/chapter2/Chapter2Level1.cpp
@@ -33,10 +33,10 @@ Scene* Chapter2Level1::createScene()
     auto player1Score = Label::createWithTTF("00000", "Marker Felt.ttf", 32);
     auto player2Score = Label::createWithTTF("00000", "Marker Felt.ttf", 32);
     
-    labelNode->addChild(player1, 0);
-    labelNode->addChild(player2, 0);
-    labelNode->addChild(player1Score, 0);
-    labelNode->addChild(player2Score, 0);
+    labelNode->addChild(player1, -2);
+    labelNode->addChild(player1Score, -1);
+    labelNode->addChild(player2, 1);
+    labelNode->addChild(player2Score, 2);
     
     player1->setPosition(Vec2(0 + player1->getContentSize().width / 2 + paddingX,
                               visibleSize.height - player1->getContentSize().height / 2 - paddingY));
@@ -50,7 +50,7 @@ Scene* Chapter2Level1::createScene()
     player2->setPosition(Vec2(player2Score->getPositionX() - player2Score->getContentSize().width - paddingX,
                               visibleSize.height - player2Score->getContentSize().height / 2 - paddingY));
     
-    scene->addChild(labelNode, -1);
+    scene->addChild(labelNode, 0);
     
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     // create a node to hold non-sprites.
@@ -79,7 +79,7 @@ Scene* Chapter2Level1::createScene()
         
         sX += sprite->getContentSize().width;
         
-        nodeItems->addChild(sprite, 0);
+        nodeItems->addChild(sprite, -1);
     }
     
     testSprite = NULL;
@@ -107,7 +107,7 @@ Scene* Chapter2Level1::createScene()
             sprite->setName("middleBlock1");
         }
         
-        nodeItems->addChild(sprite, 0);
+        nodeItems->addChild(sprite, 1);
     }
     
     // right side blocks
@@ -127,7 +127,7 @@ Scene* Chapter2Level1::createScene()
             sprite->setName("middleBlock2");
         }
         
-        scene->addChild(sprite,0);
+        scene->addChild(sprite, 1);
     }
     
     // center blocks
@@ -142,7 +142,7 @@ Scene* Chapter2Level1::createScene()
         
         sX += sprite->getContentSize().width;
         
-        nodeItems->addChild(sprite,0);
+        nodeItems->addChild(sprite, 1);
     }
     
     testSprite = NULL;
@@ -161,7 +161,7 @@ Scene* Chapter2Level1::createScene()
     sprite1->setPosition(100, (visibleSize.height - playingSize.height));
     sprite1->setName("mainSprite");
     
-    spriteNode->addChild(sprite1, 0);
+    spriteNode->addChild(sprite1, 1);
     
     scene->addChild(spriteNode, 1);
     
@@ -198,7 +198,7 @@ Scene* Chapter2Level1::createScene()
         sprite1->setAnchorPoint(Vec2(0,0));
         sprite1->setName("childSprite1");
         
-        bnode->addChild(sprite1, 0);
+        bnode->addChild(sprite1, 1);
 
         sprite1->setPosition(dnode->getPositionX(), dnode->getPositionY());
         
@@ -206,7 +206,7 @@ Scene* Chapter2Level1::createScene()
         sprite2->setAnchorPoint(Vec2(0,0));
         sprite2->setName("childSprite2");
         
-        bnode->addChild(sprite2, 0);
+        bnode->addChild(sprite2, 1);
         
         sprite2->setPosition(dnode->getPositionX() - dnode->getContentSize().width * 3, dnode->getPositionY());
         
@@ -220,19 +220,27 @@ Scene* Chapter2Level1::createScene()
             auto anode = Director::getInstance()->getRunningScene()->getChildByName("spriteNode");
             auto bnode = anode->getChildByName("mainSprite");
             
-            auto rotateBy = RotateBy::create(2, 40.0f);
-            auto scaleBy = ScaleTo::create(2, 2.0f);
-        
-            auto seq = Sequence::create(rotateBy, scaleBy, NULL);
+            bnode->setRotation(40.0f);
             
-            bnode->runAction(seq);
+            auto delay = DelayTime::create(4.0f);
+            bnode->runAction(delay);
+            
+            bnode->setScale(2.0f);
+            
+            // You could also use an Action and Sequence combination to do this.
+            //auto rotateBy = RotateBy::create(2, 40.0f);
+            //auto scaleBy = ScaleTo::create(2, 2.0f);
+        
+            //auto seq = Sequence::create(rotateBy, scaleBy, NULL);
+            
+            //bnode->runAction(seq);
             
         }, 3, "udpate_key");
     });
 
     auto menu = Menu::create(menuItem1, NULL);
     menu->setName("menu");
-    menuNode->addChild(menu, 0);
+    menuNode->addChild(menu, 1);
     menu->setPosition(visibleSize.width / 2, visibleSize.height - menuItem1->getContentSize().height * 2);
     
     scene->addChild(menuNode, 2);
