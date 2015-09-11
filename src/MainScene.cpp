f292cc95a950e3c298062d2b0c0da33c14191811
slackmoehrle <slackmoehrle@gmail.com>
2015-09-11 09:49:34 -0400

    finished adding all chapters

--- src/MainScene.cpp
+++ src/MainScene.cpp
@@ -1,6 +1,14 @@
 #include "MainScene.h"
 
 #include "Chapter2.h"
+#include "Chapter3.h"
+#include "Chapter4.h"
+#include "Chapter5.h"
+#include "Chapter6.h"
+#include "Chapter7.h"
+#include "Chapter9.h"
+#include "Chapter12.h"
+#include "Chapter13.h"
 
 USING_NS_CC;
 
@@ -54,19 +62,59 @@ bool MainScene::init()
     auto mainmenu = Menu::create();
     int index = 2;
     
-    // add "Static sprite3D model"
-    auto itemlabel = LabelTTF::create("Chapter 2", "Arial", 24);
-    auto menuItem = MenuItemLabel::create(itemlabel);
-    menuItem->setCallback([&](cocos2d::Ref *sender) {
+    std::vector<std::string> vChapters = {"Chapter 2", "Chapter 3", "Chapter 4", "Chapter 5", "Chapter 6", "Chapter 7", "Chapter 9", "Chapter 12", "Chapter 13"};
+    
+    std::vector<MenuItemLabel*> vMenuItems;
+    
+    // create the menu items for each chaper
+    for (int i=0; i < vChapters.size(); i++)
+    {
+        auto itemLabel = LabelTTF::create(vChapters.at(i), "Arial", 24);
+        auto menuItemLabel = MenuItemLabel::create(itemLabel);
+        
+        menuItemLabel->setPosition(Vec2(origin.x+visibleSize.width/2, origin.y+visibleSize.height/2).x,
+            (Vec2(origin.x+visibleSize.width/2, origin.y+visibleSize.height).y - (++index) * 40));
+
+        vMenuItems.push_back(menuItemLabel);
+    }
+    
+    // create the callbacks for each menu item
+    vMenuItems.at(0)->setCallback([&](cocos2d::Ref *sender) {
         Director::getInstance()->replaceScene(Chapter2::createScene());
     });
-    menuItem->setPosition(Vec2(origin.x+visibleSize.width/2, origin.y+visibleSize.height/2).x,
-                          (Vec2(origin.x+visibleSize.width/2, origin.y+visibleSize.height).y - (++index) * 40));
+    vMenuItems.at(1)->setCallback([&](cocos2d::Ref *sender) {
+        Director::getInstance()->replaceScene(Chapter3::createScene());
+    });
+    vMenuItems.at(2)->setCallback([&](cocos2d::Ref *sender) {
+        Director::getInstance()->replaceScene(Chapter4::createScene());
+    });
+    vMenuItems.at(3)->setCallback([&](cocos2d::Ref *sender) {
+        auto chapter5 = Chapter5();
+        Director::getInstance()->replaceScene(chapter5.getScene1());
+    });
+    vMenuItems.at(4)->setCallback([&](cocos2d::Ref *sender) {
+        Director::getInstance()->replaceScene(Chapter6::createScene());
+    });
+    vMenuItems.at(5)->setCallback([&](cocos2d::Ref *sender) {
+        Director::getInstance()->replaceScene(Chapter7::createScene());
+    });
+    vMenuItems.at(6)->setCallback([&](cocos2d::Ref *sender) {
+        Director::getInstance()->replaceScene(Chapter9::createScene());
+    });
+    vMenuItems.at(7)->setCallback([&](cocos2d::Ref *sender) {
+        Director::getInstance()->replaceScene(Chapter12::createScene());
+    });
+    vMenuItems.at(8)->setCallback([&](cocos2d::Ref *sender) {
+        Director::getInstance()->replaceScene(Chapter13::createScene());
+    });
     
-    mainmenu->addChild(menuItem,2);
+    // now add all the menu items to the menu
+    for (int i=0; i < vMenuItems.size(); i++)
+    {
+        mainmenu->addChild(vMenuItems.at(i),2);
+    }
+    
     mainmenu->setPosition(Vec2::ZERO);
-    
-    
     
     // add main menu
     this->addChild(mainmenu, 1);
