b1eecae4972480ec63b80f1b65a3aeccfed978a3
Dale Stammen <dalestam@microsoft.com>
2016-01-21 10:12:18 -0800

    fixed double to float conversion warning

--- cpp/src/AppDelegate.cpp
+++ cpp/src/AppDelegate.cpp
@@ -41,7 +41,7 @@ bool AppDelegate::applicationDidFinishLaunching() {
     //director->setDisplayStats(true);
 
     // set FPS. the default value is 1.0/60 if you don't call this
-    director->setAnimationInterval(1.0 / 60);
+    director->setAnimationInterval(1.0f / 60);
 
     FileUtils::getInstance()->addSearchPath("res");
     FileUtils::getInstance()->addSearchPath("res/chapter2");
