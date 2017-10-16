2940f877dc1adb31ff1efdfaf961df2c12a3abdb
Jason T Slack-Moehrle <slackmoehrle@gmail.com>
2017-10-16 16:21:21 -0400

    updated v3.16

--- cpp/src/AppDelegate.cpp
+++ cpp/src/AppDelegate.cpp
@@ -1,25 +1,57 @@
+
 #include "AppDelegate.h"
 #include "MainScene.h"
 
+// #define USE_AUDIO_ENGINE 1
+// #define USE_SIMPLE_AUDIO_ENGINE 1
+
+#if USE_AUDIO_ENGINE && USE_SIMPLE_AUDIO_ENGINE
+#error "Don't use AudioEngine and SimpleAudioEngine at the same time. Please just select one in your game!"
+#endif
+
+#if USE_AUDIO_ENGINE
+#include "audio/include/AudioEngine.h"
+using namespace cocos2d::experimental;
+#elif USE_SIMPLE_AUDIO_ENGINE
+#include "audio/include/SimpleAudioEngine.h"
+using namespace CocosDenshion;
+#endif
+
 USING_NS_CC;
 
-AppDelegate::AppDelegate() {
+static cocos2d::Size designResolutionSize = cocos2d::Size(480, 320);
+static cocos2d::Size smallResolutionSize = cocos2d::Size(480, 320);
+static cocos2d::Size mediumResolutionSize = cocos2d::Size(1024, 768);
+static cocos2d::Size largeResolutionSize = cocos2d::Size(2048, 1536);
 
-}
-
-AppDelegate::~AppDelegate() 
+AppDelegate::AppDelegate()
 {
 }
 
-//if you want a different context,just modify the value of glContextAttrs
-//it will takes effect on all platforms
+AppDelegate::~AppDelegate()
+{
+#if USE_AUDIO_ENGINE
+    AudioEngine::end();
+#elif USE_SIMPLE_AUDIO_ENGINE
+    SimpleAudioEngine::end();
+#endif
+}
+
+// if you want a different context, modify the value of glContextAttrs
+// it will affect all platforms
 void AppDelegate::initGLContextAttrs()
 {
-    //set OpenGL context attributions,now can only set six attributions:
-    //red,green,blue,alpha,depth,stencil
+    // set OpenGL context attributes: red,green,blue,alpha,depth,stencil
     GLContextAttrs glContextAttrs = {8, 8, 8, 8, 24, 8};
-
+    
     GLView::setGLContextAttrs(glContextAttrs);
+}
+
+// if you want to use the package manager to install more packages,
+// don't modify or remove this function
+static int register_all_packages()
+{
+    return 0; //flag for packages manager
 }
 
 bool AppDelegate::applicationDidFinishLaunching() {
@@ -27,22 +59,42 @@ bool AppDelegate::applicationDidFinishLaunching() {
     auto director = Director::getInstance();
     auto glview = director->getOpenGLView();
     if(!glview) {
-        glview = GLViewImpl::create("Programmers Guide");
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32) || (CC_TARGET_PLATFORM == CC_PLATFORM_MAC) || (CC_TARGET_PLATFORM == CC_PLATFORM_LINUX)
+        glview = GLViewImpl::createWithRect("myTest", cocos2d::Rect(0, 0, designResolutionSize.width, designResolutionSize.height));
+#else
+        glview = GLViewImpl::create("myTest");
+#endif
         director->setOpenGLView(glview);
     }
-
-#if (CC_TARGET_PLATFORM == CC_PLATFORM_WINRT)
-    // Allows windows to be resizable on winrt platforms
-    Size frameSize = glview->getFrameSize();
-    glview->setDesignResolutionSize(frameSize.width, frameSize.height, ResolutionPolicy::SHOW_ALL);
-#endif
-
+    
     // turn on display FPS
-    //director->setDisplayStats(true);
-
+    director->setDisplayStats(false);
+    
     // set FPS. the default value is 1.0/60 if you don't call this
     director->setAnimationInterval(1.0f / 60);
-
+    
+    // Set the design resolution
+    glview->setDesignResolutionSize(designResolutionSize.width, designResolutionSize.height, ResolutionPolicy::NO_BORDER);
+    auto frameSize = glview->getFrameSize();
+    // if the frame's height is larger than the height of medium size.
+    if (frameSize.height > mediumResolutionSize.height)
+    {
+        director->setContentScaleFactor(MIN(largeResolutionSize.height/designResolutionSize.height, largeResolutionSize.width/designResolutionSize.width));
+    }
+    // if the frame's height is larger than the height of small size.
+    else if (frameSize.height > smallResolutionSize.height)
+    {
+        director->setContentScaleFactor(MIN(mediumResolutionSize.height/designResolutionSize.height, mediumResolutionSize.width/designResolutionSize.width));
+    }
+    // if the frame's height is smaller than the height of medium size.
+    else
+    {
+        director->setContentScaleFactor(MIN(smallResolutionSize.height/designResolutionSize.height, smallResolutionSize.width/designResolutionSize.width));
+    }
+    
+    register_all_packages();
+    
+    
     FileUtils::getInstance()->addSearchPath("res");
     FileUtils::getInstance()->addSearchPath("res/chapter2");
     FileUtils::getInstance()->addSearchPath("res/chapter3");
@@ -55,25 +107,33 @@ bool AppDelegate::applicationDidFinishLaunching() {
     
     // create a scene. it's an autorelease object
     auto scene = MainScene::createScene();
-
+    
     // run
     director->runWithScene(scene);
-
+    
     return true;
 }
 
-// This function will be called when the app is inactive. When comes a phone call,it's be invoked too
+// This function will be called when the app is inactive. Note, when receiving a phone call it is invoked.
 void AppDelegate::applicationDidEnterBackground() {
     Director::getInstance()->stopAnimation();
-
-    // if you use SimpleAudioEngine, it must be pause
-    // SimpleAudioEngine::getInstance()->pauseBackgroundMusic();
+    
+#if USE_AUDIO_ENGINE
+    AudioEngine::pauseAll();
+#elif USE_SIMPLE_AUDIO_ENGINE
+    SimpleAudioEngine::getInstance()->pauseBackgroundMusic();
+    SimpleAudioEngine::getInstance()->pauseAllEffects();
+#endif
 }
 
 // this function will be called when the app is active again
 void AppDelegate::applicationWillEnterForeground() {
     Director::getInstance()->startAnimation();
-
-    // if you use SimpleAudioEngine, it must resume here
-    // SimpleAudioEngine::getInstance()->resumeBackgroundMusic();
+    
+#if USE_AUDIO_ENGINE
+    AudioEngine::resumeAll();
+#elif USE_SIMPLE_AUDIO_ENGINE
+    SimpleAudioEngine::getInstance()->resumeBackgroundMusic();
+    SimpleAudioEngine::getInstance()->resumeAllEffects();
+#endif
 }
--- cpp/src/AppDelegate.h
+++ cpp/src/AppDelegate.h
@@ -4,35 +4,35 @@
 #include "cocos2d.h"
 
 /**
-@brief    The cocos2d Application.
-
-The reason for implement as private inheritance is to hide some interface call by Director.
-*/
+ @brief    The cocos2d Application.
+ 
+ Private inheritance here hides part of interface from Director.
+ */
 class  AppDelegate : private cocos2d::Application
 {
 public:
     AppDelegate();
     virtual ~AppDelegate();
-
+    
     virtual void initGLContextAttrs();
-
+    
     /**
-    @brief    Implement Director and Scene init code here.
-    @return true    Initialize success, app continue.
-    @return false   Initialize failed, app terminate.
-    */
+     @brief    Implement Director and Scene init code here.
+     @return true    Initialize success, app continue.
+     @return false   Initialize failed, app terminate.
+     */
     virtual bool applicationDidFinishLaunching();
-
+    
     /**
-    @brief  The function be called when the application enter background
-    @param  the pointer of the application
-    */
+     @brief  Called when the application moves to the background
+     @param  the pointer of the application
+     */
     virtual void applicationDidEnterBackground();
-
+    
     /**
-    @brief  The function be called when the application enter foreground
-    @param  the pointer of the application
-    */
+     @brief  Called when the application reenters the foreground
+     @param  the pointer of the application
+     */
     virtual void applicationWillEnterForeground();
 };
 
